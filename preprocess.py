08e455965a31857964885a1971d27e6ff1729c5d
Harshitha <kommojiharshitha@gmail.com>
2026-08-17 11:11:40 +0530

    Fix NLTK tokenizer for Streamlit deployment

--- preprocess.py
+++ preprocess.py
@@ -14,7 +14,7 @@ def preprocess_text(text):
     text = str(text).lower()
     text = re.sub(r'http\S+|www\S+', '', text)
     text = text.translate(str.maketrans('', '', string.punctuation))
-    tokens = nltk.word_tokenize(text)
+    tokens = nltk.word_tokenize(text, preserve_line=True)
     tokens = [w for w in tokens if w.isalpha() and w not in stopwords.words('english')]
     tokens = [ps.stem(w) for w in tokens]
     return ' '.join(tokens)
