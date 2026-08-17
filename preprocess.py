f1a96e3f264848769ecae50bb77bb620872f6e96
Harshitha <kommojiharshitha@gmail.com>
2026-08-17 23:07:45 +0530

    Update preprocessing and requirements

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,3 @@
-
 import re
 import string
 import nltk
@@ -6,6 +5,7 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
 nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('stopwords')
 
 ps = PorterStemmer()
